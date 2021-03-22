e4b9cedbb089e20f91c2c1a7e01c6c1a24823cca
Vlad <lavyshvlad1@gmail.com>
2021-03-22 14:00:28 +0100

    Init

---
+++ src/Form.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "client-form"
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: "Roboto", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
